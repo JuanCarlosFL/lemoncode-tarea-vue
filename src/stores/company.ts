57316f471f54ae52cc3fadac0c3b32b865f1a959
Juan Carlos <jcarlos@fuentesdev.es>
2022-12-12 10:48:37 +0100

    lemoncode by default

--- src/stores/company.ts
+++ src/stores/company.ts
@@ -2,7 +2,7 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCompanyStore = defineStore('company', () => {
-  const company = ref('');
+  const company = ref('lemoncode');
   
   return { company }
 });
