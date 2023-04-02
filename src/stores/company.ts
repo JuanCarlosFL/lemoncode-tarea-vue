0890b925be76cfaba28e22b827faac52d0d28efc
Juan Carlos <jcarlos@fuentesdev.es>
2023-04-02 09:41:33 +0200

    upload files

--- src/stores/company.ts
+++
@@ -1,9 +0,0 @@
-import { ref } from 'vue'
-import { defineStore } from 'pinia'
-
-export const useCompanyStore = defineStore('company', () => {
-  const company = ref('lemoncode');
-  
-  return { company }
-});
-
